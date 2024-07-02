61a7d4d4231fe2f921dfa148d0413c24cbcce5eb
Mahtab <azimimahtab090@gmail.com>
2024-07-02 01:28:52 -0700

    Add changes

---
+++ index.php
@@ -0,0 +1,237 @@
+  <?php
+  include("header.html")
+  ?>
+      <div id="demo" class="carousel slide" data-ride="carousel">
+
+        <!-- Indicators -->
+        <ul class="carousel-indicators">
+          <li data-target="#demo" data-slide-to="0" class="active"></li>
+          <li data-target="#demo" data-slide-to="1"></li>
+          <li data-target="#demo" data-slide-to="2"></li>
+          <li data-target="#demo" data-slide-to="3"></li>
+          <li data-target="#demo" data-slide-to="4"></li>
+        </ul>
+        
+        <!-- The slideshow -->
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="images/Header16-scaled.jpg" alt="Los Angeles" width="1100" height="500">
+          </div>
+          <div class="carousel-item">
+            <img src="images/Header17-scaled.jpg" alt="Chicago" width="1100" height="500">
+          </div>
+          <div class="carousel-item">
+            <img src="images/Header13-1-scaled.jpg" alt="Chicago" width="1100" height="500">
+          </div>
+          <div class="carousel-item">
+            <img src="images/Header15-scaled.jpg" alt="New York" width="1100" height="500">
+          </div>
+          <div class="carousel-item">
+            <img src="images/Header18-scaled.jpg" alt="Chicago" width="1100" height="500">
+          </div>
+        </div>
+        
+        <!-- Left and right controls -->
+        <a class="carousel-control-prev" href="#demo" data-slide="prev">
+          <span class="carousel-control-prev-icon"></span>
+        </a>
+        <a class="carousel-control-next" href="#demo" data-slide="next">
+          <span class="carousel-control-next-icon"></span>
+        </a>
+      </div>
+
+
+      
+
+
+
+      <div class="container">
+        <h3 class="h3" style="padding: 10px; text-decoration-line: underline;text-decoration-color:orange;font-weight:bolder;">کتاب های همیشه خواندنی</h3>
+        <div class="row">
+            <div class="col-md-3 col-sm-6">
+                <div class="product-grid2">
+                    <div class="product-image2">
+                        <a href="#">
+                            <img class="pic-1" src="images/t355_02-1.jpg">
+                            <img class="pic-2" src="images/t355_03-2-600x824.jpg">
+                        </a>
+                        <ul class="social">
+                            <li><a href="#" data-tip="Quick View"><i class="fa fa-eye"></i></a></li>
+                            <li><a href="#" data-tip="Add to Wishlist"><i class="fa fa-shopping-bag"></i></a></li>
+                            <li><a href="#" data-tip="Add to Cart"><i class="fa fa-shopping-cart"></i></a></li>
+                        </ul>
+                        <a class="add-to-cart" href="">Add to cart</a>
+                    </div>
+                    <div class="product-content">
+                        <h3 class="title" style="font-size:25px;text-align:right;font-weight:bold;"><a href="#" style="color:orange;">ماه پنهان است</a></h3>
+                        <p style="font: size 15px;font-weight:normal;color:black;text-align:right;">نویسنده: <span style="font-weight:bold;"> جان استاین‌بک</span></p>
+                        <span class="price" style="text-align:left;align-items: left;align-self: left;align-content: left;font-weight:normal;text-direction:rtl;fontsize:15px;">35,000 تومان</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="product-grid2">
+                  <div class="product-image2">
+                      <a href="#">
+                          <img class="pic-1" src="images/zash.jpeg">
+                          <img class="pic-2" src="images/zas.jpg">
+                      </a>
+                      <ul class="social">
+                          <li><a href="#" data-tip="Quick View"><i class="fa fa-eye"></i></a></li>
+                          <li><a href="#" data-tip="Add to Wishlist"><i class="fa fa-shopping-bag"></i></a></li>
+                          <li><a href="#" data-tip="Add to Cart"><i class="fa fa-shopping-cart"></i></a></li>
+                      </ul>
+                      <a class="add-to-cart" href="">Add to cart</a>
+                    </div>
+                    <div class="product-content">
+                        <h3 class="title"style="color:orange;font-size:25px;text-align:right;font-weight:bold;" ><a href="#" style="color:orange;">دیوارگذر</a></h3>
+                        <p style="font: size 15px;font-weight:normal;color:black;text-align:right;">نویسنده: <span style="font-weight:bold;">مارسل امه</span></p>
+                        <span class="price" style="text-align:left;align-items: left;align-self: left;align-content: left;font-weight:normal;text-direction:rtl;fontsize:15px;">11,000 تومان</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="product-grid2">
+                    <div class="product-image2">
+                        <a href="#">
+                            <img class="pic-1" src="images/t379_02-1.jpg">
+                            <img class="pic-2" src="images/t379_03-2.jpg">
+                        </a>
+                        <ul class="social">
+                            <li><a href="#" data-tip="Quick View"><i class="fa fa-eye"></i></a></li>
+                            <li><a href="#" data-tip="Add to Wishlist"><i class="fa fa-shopping-bag"></i></a></li>
+                            <li><a href="#" data-tip="Add to Cart"><i class="fa fa-shopping-cart"></i></a></li>
+                        </ul>
+                        <a class="add-to-cart" href="">Add to cart</a>
+                    </div>
+                    <div class="product-content">
+                        <h3 class="title" style="color:orange;font-size:25px;text-align:right;font-weight:bold;"><a href="#" style="color:orange;">سوفیا پتروونا</a></h3>
+                        <p style="font: size 15px;font-weight:normal;color:black;text-align:right;">نویسنده: <span style="font-weight:bold;"> لیدیا چوکوفسکایا</span></p>
+                        <span class="price" style="text-align:left;align-items: left;align-self: left;align-content: left;font-weight:normal;text-direction:rtl;fontsize:15px;">37,000 تومان</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="product-grid2">
+                    <div class="product-image2">
+                        <a href="#">
+                            <img class="pic-1" src="images/t346_02-1.jpg">
+                            <img class="pic-2" src="images/t346_03-2-600x825.jpg">
+                        </a>
+                        <ul class="social">
+                            <li><a href="#" data-tip="Quick View"><i class="fa fa-eye"></i></a></li>
+                            <li><a href="#" data-tip="Add to Wishlist"><i class="fa fa-shopping-bag"></i></a></li>
+                            <li><a href="#" data-tip="Add to Cart"><i class="fa fa-shopping-cart"></i></a></li>
+                        </ul>
+                        <a class="add-to-cart" href="">Add to cart</a>
+                    </div>
+                    <div class="product-content">
+                        <h3 class="title" style="color:orange;font-size:25px;text-align:right;font-weight:bold;"><a href="#"style="color:orange;" >یک رمانک لمپن</a></h3>
+                        <p style="font: size 15px;font-weight:normal;color:black;text-align:right;">نویسنده: <span style="font-weight:bold;">روبرتو بولانیو </span></p>
+                        <span class="price" style="text-align:left;align-items: left;align-self: left;align-content: left;font-weight:normal;text-direction:rtl;fontsize:15px;">27,000 تومان</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="container">
+        <h3 class="h3"  style="padding: 10px; text-decoration-line: underline;text-decoration-color:orange;font-weight:bold;">تازه های مرداد</h3>
+        <div class="row">
+            <div class="col-md-3 col-sm-6">
+                <div class="product-grid2">
+                    <div class="product-image2">
+                        <a href="#">
+                            <img class="pic-1" src="images/roo.jpg">
+                            <img class="pic-2" src="images/posht.jpg">
+                        </a>
+                        <ul class="social">
+                            <li><a href="#" data-tip="Quick View"><i class="fa fa-eye"></i></a></li>
+                            <li><a href="#" data-tip="Add to Wishlist"><i class="fa fa-shopping-bag"></i></a></li>
+                            <li><a href="#" data-tip="Add to Cart"><i class="fa fa-shopping-cart"></i></a></li>
+                        </ul>
+                        <a class="add-to-cart" href="">Add to cart</a>
+                    </div>
+                    <div class="product-content">
+                        <h3 class="title" style="color:orange;font-size:25px;text-align:right;font-weight:bold;"><a href="#" style="color:orange;">بارادین</a></h3>
+                        <p style="font: size 15px;font-weight:normal;color:black;text-align:right;">نویسنده: <span style="font-weight:bold;">نینا بربروا</span></p>
+                        <span class="price" style="text-align:left;align-items: left;align-self: left;align-content: left;font-weight:normal;text-direction:rtl;fontsize:15px;">15,000 تومان</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="product-grid2">
+                  <div class="product-image2">
+                      <a href="#">
+                          <img class="pic-1" src="images/t46_02-1.jpg">
+                          <img class="pic-2" src="images/t46_03-1-600x825.jpg">
+                      </a>
+                      <ul class="social">
+                          <li><a href="#" data-tip="Quick View"><i class="fa fa-eye"></i></a></li>
+                          <li><a href="#" data-tip="Add to Wishlist"><i class="fa fa-shopping-bag"></i></a></li>
+                          <li><a href="#" data-tip="Add to Cart"><i class="fa fa-shopping-cart"></i></a></li>
+                      </ul>
+                      <a class="add-to-cart" href="">Add to cart</a>
+                    </div>
+                    <div class="product-content">
+                        <h3 class="title"style="color:orange;font-size:25px;text-align:right;font-weight:bold;" ><a href="#"style="color:orange;" >دوست بازیافته</a></h3>
+                        <p style="font: size 15px;font-weight:normal;color:black;text-align:right;">نویسنده: <span style="font-weight:bold;">فرد اولمن</span></p>
+                        <span class="price" style="text-align:left;align-items: left;align-self: left;align-content: left;font-weight:normal;text-direction:rtl;fontsize:15px;">55,000 تومان</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="product-grid2">
+                    <div class="product-image2">
+                        <a href="#">
+                            <img class="pic-1" src="images/ktbmdr_ca.jpg">
+                            <img class="pic-2" src="images/ktbmdr_cb-600x825.jpg">
+                        </a>
+                        <ul class="social">
+                            <li><a href="#" data-tip="Quick View"><i class="fa fa-eye"></i></a></li>
+                            <li><a href="#" data-tip="Add to Wishlist"><i class="fa fa-shopping-bag"></i></a></li>
+                            <li><a href="#" data-tip="Add to Cart"><i class="fa fa-shopping-cart"></i></a></li>
+                        </ul>
+                        <a class="add-to-cart" href="">Add to cart</a>
+                    </div>
+                    <div class="product-content">
+                        <h3 class="title" style="color:orange;font-size:25px;text-align:right;font-weight:bold;"><a href="#" style="color:orange;">کتاب مادرم</a></h3>
+                        <p style="font: size 15px;font-weight:normal;color:black;text-align:right;">نویسنده: <span style="font-weight:bold;">آلبر کوئن</span></p>
+                        <span class="price" style="text-align:left;align-items: left;align-self: left;align-content: left;font-weight:normal;text-direction:rtl;fontsize:15px;">40,000 تومان</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="product-grid2">
+                    <div class="product-image2">
+                        <a href="#">
+                            <img class="pic-1" src="images/t44_02-1.jpg">
+                            <img class="pic-2" src="images/t44_03-2-600x825.jpg">
+                        </a>
+                        <ul class="social">
+                            <li><a href="#" data-tip="Quick View"><i class="fa fa-eye"></i></a></li>
+                            <li><a href="#" data-tip="Add to Wishlist"><i class="fa fa-shopping-bag"></i></a></li>
+                            <li><a href="#" data-tip="Add to Cart"><i class="fa fa-shopping-cart"></i></a></li>
+                        </ul>
+                        <a class="add-to-cart" href="">Add to cart</a>
+                    </div>
+                    <div class="product-content">
+                        <h3 class="title" style="color:orange;font-size:25px;text-align:right;font-weight:bold;color:orange;!important"><a href="#" style="color:orange;">قاضی و جلادش</a></h3>
+                        <p style="font: size 15px;font-weight:normal;color:black;text-align:right;">نویسنده: <span style="font-weight:bold;">فریدریش دورنمات </span></p>
+                        <span class="price" style="text-align:left;align-items: left;align-self: left;align-content: left;font-weight:normal;text-direction:rtl;fontsize:15px;">45,000 تومان</span>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+
+    <hr>
+    <hr>
+   <?php
+   include("footer.html") 
+   ?>
+</body>
+</html>
